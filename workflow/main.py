40463cd1ae5f67467e59e167967c61234721d28b
narsimrao <narsim498@gmail.com>
2022-04-29 06:39:01 +0530

    workflow

---
+++ workflow/main.py
@@ -0,0 +1,8 @@
+import os
+
+# notebook_files = []
+# for file in os.listdir('./workflow'):
+#     if file.endswith(".ipynb"):
+#         print(os.path.join("/mydir", file))
+#         notebook_files.append(os.path.join("/mydir", file))
+
